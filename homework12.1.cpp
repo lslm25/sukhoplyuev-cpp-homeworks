3ad4f370c420a1e1ef71e56ce4e8d13bd0b27bf3
lslm25 <115701357+lslm25@users.noreply.github.com>
2025-12-14 00:18:45 +0500

    Create homework12.1.cpp

---
+++ homework12.1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int age;
+    double score;
+
+public:
+
+    Student() {
+        name = "";
+        age = 0;
+        score = 0.0;
+    }
+    Student(string n, int a, double s) {
+        name = n;
+        age = a;
+        score = s;
+    }
+    void setName(const string& n) {
+        name = n;
+    }
+    void setAge(int a) {
+        age = a;
+    }
+    void setScore(double s) {
+        score = s;
+    }
+    string getName() const {
+        return name;
+    }
+    int getAge() const {
+        return age;
+    }
+    double getScore() const {
+        return score;
+    }
+    void show() const {
+        cout << "Имя: " << name << endl;
+        cout << "Возраст: " << age << endl;
+        cout << "Средний балл: " << score << endl;
+    }
+};
+
+int main() {
+    Student st("Иван", 19, 4.2);
+    st.show();
+    return 0;
+}
